38573ae1303cf01cbba4a66a453f45e7684ba433
Oscar Gillespie <oscar.e.gillespie@gmail.com>
2019-01-19 16:09:38 +0000

    the points on the map now can be clicked to bring up an "about place" view on rhs

--- public/scripts/outlets.js
+++ public/scripts/outlets.js
@@ -2,10 +2,16 @@
 var outlets = [{
     name: "Arjuna",
     lat: 52.2004,
-    lng: 0.1338
+    lng: 0.1338,
+    photos: "no photos",
+    description: "here is some tempeh and miso",
+    reviews: "5 out of 5"
 },
 {
     name: "Espresso Library",
     lat: 52.2036,
-    lng: 0.1322
+    lng: 0.1322,
+    photos: "no photos",
+    description: "here is some coffee",
+    reviews: "5 out of 5"
 }]
